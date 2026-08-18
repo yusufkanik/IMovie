824ad1ccfb24cb18eb867390b12ee2b25e8babda
yusufkanik <kanikyusuf2004@gmail.com>
2026-08-18 14:41:33 +0300

    Movie DTO added

---
+++ DTOs/TmdbMovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+namespace MovieAPI.DTOs
+
+{
+    public class TmdbMovieDto
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("original_title")]
+        public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("overview")]
+        public string Overview { get; set; } = string.Empty;
+
+        [JsonPropertyName("poster_path")]
+        public string? PosterPath {  get; set; }
+
+        [JsonPropertyName("release_name")]
+        public string? ReleaseDate { get; set; }
+
+        [JsonPropertyName("vote_average")]
+        public double VoteAverage { get; set; }
+
+        [JsonPropertyName("vote_count")]
+        public int VoteCount { get; set; }
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,12 +3,15 @@
     public class Movie
     {
         public int Id { get; set; }
+        public int TmdbId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Overview { get; set; } = string.Empty;
         public string? PosterPath { get; set; }
         public string? ReleaseDate { get; set; }
         public double VoteAverage { get; set; }
-        public int? VoteCount { get; set; }
+        public int VoteCount { get; set; }
+        public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedAtUtc { get; private set; } = DateTime.UtcNow;
 
     }
 }
