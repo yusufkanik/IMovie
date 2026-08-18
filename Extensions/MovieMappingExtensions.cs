97bea1c450bc7ef801a8002610a1f08d5b021292
yusufkanik <kanikyusuf2004@gmail.com>
2026-08-18 15:58:38 +0300

    Extensions that converts DTOs to movies added

---
+++ Extensions/MovieMappingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using MovieAPI.DTOs;
+using MovieAPI.Models;
+using System.Collections;
+
+namespace MovieAPI.Extensions
+{
+    public static class MovieMappingExtensions
+    {
+        // convert the DTO object to Movie object
+        public static Movie ToModel(this TmdbMovieDto dto)
+        {
+            return new Movie
+            {
+                TmdbId = dto.Id,
+                Title = dto.Title,
+                Overview = dto.Overview,
+                PosterPath = dto.PosterPath,
+                ReleaseDate = dto.ReleaseDate,
+                VoteAverage = dto.VoteAverage,
+                VoteCount = dto.VoteCount,
+            };
+        }
+
+        // convert a list of DTOs to a list of Movie objects
+        public static List<Movie> ToModelList(this IEnumerable<TmdbMovieDto> dtos)
+        {
+            return dtos.Select(dto => dto.ToModel()).ToList();
+        }
+    }
+}
